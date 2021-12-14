ec44fa8bebf1dc68e6a121fadad395f29235fefc
Kara Rainey <kara@brivity.com>
2021-12-14 03:50:40 -0600

    testing git in VSCode

--- src/Components/ProductPage/ProductPage.js
+++ src/Components/ProductPage/ProductPage.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import TestImage1 from "../Logos/slammer1.jpeg";
 
 export default class ProductPage extends Component{
-// Product Page should have Multiple images of product, Price, Option to add to cart. Page should show return policy, product discriptions, Pre-order cutoff date, links to other products and categories.
+// Product Page should have Multiple images of product, Price, Option to add to cart. Page should show return policy, product discriptions, Pre-order cutoff date, links to other products and categories. Testing Git
 
   render(){
     return(
